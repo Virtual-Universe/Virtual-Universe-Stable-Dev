cd4d93ad53b7f4f4ee5e80d35c0be6fcbf80bcb0
QueenStarfinder <queen@secondgalaxy.com>
2016-07-28 20:05:07 -0400

    Cleanup BasicPhysics
    - Fix licensing as necessary
    - Cleanup unnecessary white spaces
    - cleanup commented out code that is either no longer used or depreciated.
    - CLeanup trailing whitespaces as necessary
    
    Signed-Off-By: QueenStarfinder<queen@secondgalaxy.com>

--- Universe/Physics/BasicPhysicsPlugin/BasicPhysicsActor.cs
+++ Universe/Physics/BasicPhysicsPlugin/BasicPhysicsActor.cs
@@ -96,6 +96,7 @@ namespace Universe.Physics.BasicPhysicsPlugin
         public override bool Flying { get; set; }
 
         public override bool IsTruelyColliding { get; set; }
+
         public override bool IsColliding { get; set; }
 
         public override Vector3 Position { get; set; }
@@ -216,6 +217,7 @@ namespace Universe.Physics.BasicPhysicsPlugin
         }
 
         public override bool IsTruelyColliding { get; set; }
+
         public override bool IsColliding { get; set; }
 
         public override Vector3 Position { get; set; }
